c51fb14fc935a7bcf3448ebca25ffd3cc6915ee2
Pei Yang Ching <59727193+horriblename@users.noreply.github.com>
2024-07-14 12:55:09 +0200

    assert: add test helpers

--- src/assert/assert.go
+++ src/assert/assert.go
@@ -7,25 +7,30 @@ import (
 
 func Ok(e error, msg ...any) {
 	if e != nil {
-		panic(fmt.Errorf("assertion failed (got error): %s\n%s", e, join(msg)))
+		panic(fmt.Errorf("assertion failed (got error): %s\n%s", e, joinHint(msg)))
 	}
 }
 
 func Eq[T comparable](a T, b T, msg ...any) {
 	if a != b {
-		panic(fmt.Errorf("failed assertion a == b:\n  left: %v\n  right: %v\nHint: %s", a, b,
-			join(msg)))
+		panic(fmt.Errorf("failed assertion a == b:\n  left: %v\n  right: %v\n%s", a, b,
+			joinHint(msg)))
 	}
 }
 
 func True(b bool, msg ...any) {
 	if !b {
-		panic(fmt.Errorf("failed assertion\n%s", join(msg)))
+		panic(fmt.Errorf("failed assertion\n%s", joinHint(msg)))
 	}
 }
 
-func join(msg ...any) string {
+func joinHint(msg ...any) string {
+	if len(msg) == 0 {
+		return ""
+	}
+
 	b := strings.Builder{}
+	b.WriteString("Hint: ")
 
 	for m := range msg {
 		b.WriteString(fmt.Sprintf("%v", m))
---
+++ src/assert/tests.go
@@ -0,0 +1,29 @@
+package assert
+
+import (
+	"testing"
+)
+
+type TestAsserts struct{ t *testing.T }
+
+func (t *TestAsserts) Ok(e error, msg ...any) {
+	t.t.Helper()
+	if e != nil {
+		t.t.Fatalf("assertion failed (got error): %s\n%s", e, joinHint(msg))
+	}
+}
+
+func (t *TestAsserts) Eq(a any, b any, msg ...any) {
+	t.t.Helper()
+	if a != b {
+		t.t.Fatalf("failed assertion a == b: \n  left: %v\n  right: %v\n%s", a, b,
+			joinHint(msg))
+	}
+}
+
+func (t *TestAsserts) True(b bool, msg ...any) {
+	t.t.Helper()
+	if !b {
+		t.t.Fatalf("failed assertion\n%s", joinHint(msg))
+	}
+}
