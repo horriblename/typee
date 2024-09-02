e9f7c893dd2f3e44ee4e600d546ea403a91e40d1
Pei Yang Ching <59727193+horriblename@users.noreply.github.com>
2024-09-02 17:03:03 +0200

    add functional utils

---
+++ src/fun/util.go
@@ -0,0 +1,12 @@
+// functional utils
+package fun
+
+func Map[T, U any](xs []T, f func(T) U) []U {
+	ys := make([]U, 0, len(xs))
+
+	for _, x := range xs {
+		ys = append(ys, f(x))
+	}
+
+	return ys
+}
