929f35481128191c551d7cd86a63bd19af3d09a8
Ching Pei Yang <badnam3o.0@gmail.com>
2024-10-14 02:44:27 +0200

    biunify: optimise bindings.go

--- src/biunify/bindings.go
+++ src/biunify/bindings.go
@@ -1,38 +1,55 @@
 package biunify
 
-import "github.com/horriblename/typee/src/opt"
+import (
+	"github.com/horriblename/typee/src/opt"
+)
+
+type bindChange struct {
+	Key   string
+	Value opt.Option[Value]
+}
 
 type Bindings struct {
-	m []map[string]Value
+	m          map[string]Value
+	changes    []bindChange
+	savePoints []int
 }
 
 func NewBindings() Bindings {
-	return Bindings{m: []map[string]Value{{}}}
+	return Bindings{m: map[string]Value{}, changes: []bindChange{}}
 }
 
 func (self *Bindings) get(k string) opt.Option[Value] {
-	for i := len(self.m) - 1; i > 0; i-- {
-		if v, found := self.m[i][k]; found {
-			return opt.Some(v)
-		}
+	if v, found := self.m[k]; found {
+		return opt.Some(v)
 	}
 
 	return opt.None[Value]()
 }
 
 func (self *Bindings) insert(k string, v Value) {
-	if len(self.m) == 0 {
-		panic("assertion failed: tried to insert into Bindings with no scopes")
+	change := bindChange{Key: k}
+	if old, found := self.m[k]; found {
+		change.Value = opt.Some(old)
 	}
-
-	self.m[len(self.m)-1][k] = v
+	self.changes = append(self.changes, change)
+	self.m[k] = v
 }
 
 func (self *Bindings) NewScope() {
-	self.m = append(self.m, map[string]Value{})
+	self.savePoints = append(self.savePoints, len(self.changes))
 }
 
 func (self *Bindings) PopScope() {
-	self.m[len(self.m)-1] = nil
-	self.m = self.m[:len(self.m)-1]
+	savePoint := self.savePoints[len(self.savePoints)-1]
+	for i := len(self.changes) - 1; i >= savePoint; i-- {
+		change := self.changes[i]
+		if old, found := change.Value.Unwrap(); found {
+			self.m[change.Key] = old
+		} else {
+			delete(self.m, change.Key)
+		}
+	}
+	self.savePoints = self.savePoints[:len(self.savePoints)-1]
+	// TODO: should shrink savePoints
 }
