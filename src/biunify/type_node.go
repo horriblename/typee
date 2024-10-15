c126bcfe9f6e05a47c59382e609be7d14adce2b6
Ching Pei Yang <badnam3o.0@gmail.com>
2024-10-15 03:24:57 +0200

    biunify: impl ordered set

--- src/biunify/type_node.go
+++ src/biunify/type_node.go
@@ -20,10 +20,10 @@ type unit struct{}
 type Reachability struct {
 	// all the nodes that have edges to a given node
 	// e.g. upSets[id] is the set of all nodes that can reach id
-	upSets []set[ID]
+	upSets []OrderedSet[ID]
 
 	// downSets[id] is the set of nodes we can reach from id
-	downSets []set[ID]
+	downSets []OrderedSet[ID]
 }
 
 type Edge struct {
@@ -33,7 +33,7 @@ type Edge struct {
 
 func (self *Reachability) addNode() ID {
 	id := len(self.upSets)
-	var set1, set2 set[ID]
+	var set1, set2 OrderedSet[ID]
 	set1.insert(id)
 	set2.insert(id)
 
@@ -48,15 +48,15 @@ func (self *Reachability) addEdge(lhs ID, rhs ID, out *[]Edge) {
 	}
 
 	// Get all ancestores of lhs, including lhs itself
-	lhsSet := make([]ID, 0, len(self.upSets[lhs]))
-	for id, _ := range self.upSets[lhs] {
+	lhsSet := make([]ID, 0, self.upSets[lhs].len())
+	for _, id := range self.upSets[lhs].slice() {
 		lhsSet = append(lhsSet, id)
 	}
 	slices.Sort(lhsSet)
 
 	// Get all descendants of rhs, including rhs itself
-	rhsSet := make([]ID, 0, len(self.downSets[rhs]))
-	for id, _ := range self.downSets[rhs] {
+	rhsSet := make([]ID, 0, self.downSets[rhs].len())
+	for _, id := range self.downSets[rhs].slice() {
 		rhsSet = append(rhsSet, id)
 	}
 	slices.Sort(rhsSet)
@@ -71,27 +71,30 @@ func (self *Reachability) addEdge(lhs ID, rhs ID, out *[]Edge) {
 	}
 }
 
-type set[T comparable] map[T]struct{}
+type OrderedSet[T comparable] struct {
+	list    []T
+	mapping map[T]struct{}
+}
 
-func (set set[T]) insert(x T) (existed bool) {
-	_, existed = set[x]
+func (set OrderedSet[T]) insert(x T) (existed bool) {
+	_, existed = set.mapping[x]
 	if !existed {
-		set[x] = struct{}{}
+		set.mapping[x] = struct{}{}
+		set.list = append(set.list, x)
 	}
 
 	return existed
 }
 
-func (set set[T]) remove(x T) (existed bool) {
-	_, existed = set[x]
-	if existed {
-		delete(set, x)
-	}
-
-	return existed
-}
-
-func (set set[T]) has(x T) bool {
-	_, found := set[x]
+func (set OrderedSet[T]) has(x T) bool {
+	_, found := set.mapping[x]
 	return found
+}
+
+func (set OrderedSet[T]) slice() []T {
+	return set.list
+}
+
+func (set OrderedSet[T]) len() int {
+	return len(set.list)
 }
