b7a3404cb16feca861f4fc4102da48d51415a881
Ching Pei Yang <badnam3o.0@gmail.com>
2024-10-16 01:06:07 +0200

    biunify: export reachability graph to graphviz

---
+++ src/biunify/graphviz.go
@@ -0,0 +1,43 @@
+package biunify
+
+import (
+	"fmt"
+	"io"
+)
+
+func ExportReachability(r *Reachability, types []TypeNode, w io.Writer) (err error) {
+	defer func() {
+		er := recover()
+		if e, ok := er.(internalError); ok {
+			err = e
+		} else if er != nil {
+			panic(e)
+		}
+	}()
+
+	checkWrite(w.Write([]byte("digraph Types {\n")))
+	checkWrite(io.WriteString(w, "// Node Labels\n"))
+
+	for id, ty := range types {
+		checkWrite(fmt.Fprintf(w, "%d [label=<<i>%#v</i>>]\n", id, ty))
+	}
+
+	checkWrite(io.WriteString(w, "// Graph\n"))
+	for from, downSet := range r.downSets {
+		for _, to := range downSet.slice() {
+			checkWrite(fmt.Fprintf(w, "%d -> %d\n", from, to))
+		}
+	}
+
+	checkWrite(w.Write([]byte("}\n")))
+
+	return nil
+}
+
+type internalError error
+
+func checkWrite(_ int, err error) {
+	if err != nil {
+		panic(internalError(err))
+	}
+}
