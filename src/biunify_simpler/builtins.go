565502e5cfb5e19cf5b2097a56cee0ed1f740617
Ching Pei Yang <badnam3o.0@gmail.com>
2024-10-26 15:50:24 +0200

    biunify-simpler: add builtins

---
+++ src/biunify_simpler/builtins.go
@@ -0,0 +1,27 @@
+package biunify
+
+import "github.com/horriblename/typee/src/internal/scope"
+
+var intBinaryOptType = Func{
+	Args: []SimpleType{Int{}, Int{}},
+	Ret:  Int{},
+}
+
+var intComparatorType = Func{
+	Args: []SimpleType{Int{}, Int{}},
+	Ret:  Bool{},
+}
+
+func addBuiltins(scope scope.ScopedMap[TypeScheme]) {
+	scope.Insert("+", intBinaryOptType)
+	scope.Insert("-", intBinaryOptType)
+	scope.Insert("*", intBinaryOptType)
+	scope.Insert("/", intBinaryOptType)
+	scope.Insert(">", intComparatorType)
+	scope.Insert("<", intComparatorType)
+	scope.Insert("=", intComparatorType)
+	scope.Insert("print", Func{
+		Args: []SimpleType{Str{}},
+		Ret:  Str{},
+	})
+}
