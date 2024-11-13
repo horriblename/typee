056de6e61f43dd6304b164847323eca1600f8266
Ching Pei Yang <badnam3o.0@gmail.com>
2024-11-13 21:16:41 +0100

    simplesub: add class def test

--- src/simplesub/simplesub_test.go
+++ src/simplesub/simplesub_test.go
@@ -149,6 +149,34 @@ func TestTypeProgram(t *testing.T) {
 				&types.String{},
 			},
 		},
+		{
+			desc: "class definition",
+			input: `
+				(class Foo {x Int, pub (def id (Int Int) [x] x)})
+			`,
+			typ: []types.Type{
+				&types.Class{
+					Name:   "Foo",
+					Supers: []*types.Class{},
+					Fields: map[string]types.Member{
+						"x": {
+							Access: types.AccessPrivate,
+							Type:   &types.Int{},
+						},
+					},
+					Statics: map[string]types.Member{},
+					Methods: map[string]types.Member{
+						"id": {
+							Access: types.AccessPublic,
+							Type: &types.Func{
+								Args: []types.Type{&types.Int{}},
+								Ret:  &types.Int{},
+							},
+						},
+					},
+				},
+			},
+		},
 	}
 	for _, tC := range testCases {
 		t.Run(tC.desc, func(t *testing.T) {
