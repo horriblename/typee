38354733c35e335e1c32d0765f8f9f44acd4e9f0
Ching Pei Yang <badnam3o.0@gmail.com>
2024-11-26 17:42:01 +0100

    gir: update enum syntax

--- src/gir/gen.go
+++ src/gir/gen.go
@@ -84,7 +84,7 @@ func (self *Generator) processEnumInfo(ei *gi.EnumInfo) {
 	p("(enum %s {\n", ei.Name())
 	for i, n := 0, ei.NumValue(); i < n; i++ {
 		val := ei.Value(i)
-		p("  %s:%d,\n", snake_case_to_PascalCase(val.Name()), val.Value())
+		p("  %s:%d\n", snake_case_to_PascalCase(val.Name()), val.Value())
 	}
 	p("})\n")
 }
