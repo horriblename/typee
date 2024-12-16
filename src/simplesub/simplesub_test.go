7ace7c303d74c5603d609156c6adec4b434836fb
Ching Pei Yang <badnam3o.0@gmail.com>
2024-12-16 18:35:51 +0100

    simplesub: add alias test

--- src/simplesub/simplesub_test.go
+++ src/simplesub/simplesub_test.go
@@ -263,6 +263,32 @@ func TestTypeProgram(t *testing.T) {
 			}(),
 		},
 		{
+			desc: "type alias",
+			input: `
+				(type Foo {x: Int})
+				(def x (Foo Foo) [foo] {x: (+ foo.x 1)})
+			`,
+			typ: []types.Type{
+				&types.Record{
+					Fields: map[string]types.Type{
+						"x": &types.Int{},
+					},
+				},
+				&types.Func{
+					Args: []types.Type{&types.Record{
+						Fields: map[string]types.Type{
+							"x": &types.Int{},
+						},
+					}},
+					Ret: &types.Record{
+						Fields: map[string]types.Type{
+							"x": &types.Int{},
+						},
+					},
+				},
+			},
+		},
+		{
 			desc:  "callExtern",
 			input: "(def foo ({}) [] (callExtern exit 0))",
 			typ: []types.Type{&types.Func{
