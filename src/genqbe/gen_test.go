9f7e61fce1a750009b5817c6107fb64525671c3b
Ching Pei Yang <badnam3o.0@gmail.com>
2024-12-28 11:48:02 +0100

    qbe: rework tests

--- src/genqbe/gen_test.go
+++ src/genqbe/gen_test.go
@@ -2,9 +2,11 @@ package genqbe
 
 import (
 	"bytes"
+	"io/fs"
 	"os"
 	"os/exec"
 	"path"
+	"strings"
 	"testing"
 
 	"github.com/horriblename/typee/src/assert"
@@ -13,201 +15,26 @@ import (
 )
 
 func TestGen(t *testing.T) {
-	testCases := []struct {
-		desc   string
-		input  string
-		output string
-	}{
-		{
-			desc:  "idk",
-			input: "(def foo [x y] (+ x (+ y 1)))",
-			output: //
-			`type :Str = {l, l, }
-type :GObject = {l, l, l, }
-` + builtinsQbe + `export function l $TestModule.foo(l %x, l %y) {
-@start
-	%_tmp_1 =l add %y, 1
-	%_tmp_2 =l add %x, %_tmp_1
-	ret %_tmp_2
-}
-`,
-		},
-		{
-			desc: "class construction + type annotated method",
-			input: `
-				(class Foo {pub (def name (Str) [] "foo")})
-				(def bar (Foo Str) [foo] (foo#name))
-				(def main []
-					(print (bar (Foo.new))))
-			`,
-			output: //
-			`type :Str = {l, l, }
-type :GObject = {l, l, l, }
-type :Foo = {:GObject, l, }
-data $_tmp_1 = {b "foo"}
-data $_stdout = { l $stdout }
-function l $print(:Str %s) {
-@start
-	%str_data =l loadl %s
-	# 64-bit architecture only lul
-	%len_loc =l add %s, 8
-	%str_len =w loadw %len_loc
-	%stdout =l loadl $_stdout
-	%res =w call $fwrite(l %str_data, w 1, w %str_len, l %stdout)
-	ret 0
-}
+	entries, err := fs.ReadDir(os.DirFS("."), "tests")
+	assert.Ok(err)
 
-export function :Str $TestModule.Foo_name() {
-@start
-	%_tmp_2 =l alloc4 24
-	storel $_tmp_1, %_tmp_2
-	%_tmp_3 =l add %_tmp_2, 8
-	storel 3, %_tmp_3
-	ret %_tmp_2
-}
-export function :Str $TestModule.bar(l %foo) {
-@start
-	%_tmp_4 =:Str call $TestModule.Foo_name ()
-	ret %_tmp_4
-}
-export function w $main() {
-@start
-	%_tmp_6 =l call $malloc (l 32)
-	%_tmp_5 =:Str call $TestModule.bar (l %_tmp_6)
-	%_tmp_7 =l call $print (:Str %_tmp_5)
-	ret %_tmp_7
-}
-`,
-		},
-		{
-			desc: "class field access",
-			input: `(class Foo {pub x Int})
-				(def getX (Foo Int) [foo] foo.x)
-			`,
-			output: `type :Str = {l, l, }
-type :GObject = {l, l, l, }
-type :Foo = {:GObject, l, l, }
-data $_stdout = { l $stdout }
-function l $print(:Str %s) {
-@start
-	%str_data =l loadl %s
-	# 64-bit architecture only lul
-	%len_loc =l add %s, 8
-	%str_len =w loadw %len_loc
-	%stdout =l loadl $_stdout
-	%res =w call $fwrite(l %str_data, w 1, w %str_len, l %stdout)
-	ret 0
-}
+	for _, entry := range entries {
+		if !entry.Type().IsRegular() || !strings.HasSuffix(entry.Name(), ".hor") {
+			continue
+		}
 
-export function l $TestModule.getX(l %foo) {
-@start
-	%_tmp_1 =l add %foo, 24
-	%_tmp_2 =l loadl %_tmp_1
-	ret %_tmp_2
-}
-`,
-		},
-		{
-			desc: "enum",
-			input: `
-				(enum Foo {Ok:0 Failure:1 BadUsage:2})
-				(def main [] (exit Foo::BadUsage))
-			`,
-			output: `type :Str = {l, l, }
-type :GObject = {l, l, l, }
-data $_stdout = { l $stdout }
-function l $print(:Str %s) {
-@start
-	%str_data =l loadl %s
-	# 64-bit architecture only lul
-	%len_loc =l add %s, 8
-	%str_len =w loadw %len_loc
-	%stdout =l loadl $_stdout
-	%res =w call $fwrite(l %str_data, w 1, w %str_len, l %stdout)
-	ret 0
-}
+		name := strings.TrimSuffix(entry.Name(), ".hor")
 
-export function w $main() {
-@start
-	call $exit (l 2)
-	ret 0
-}
-`,
-		},
-		{
-			desc: "callExtern",
-			input: `
-				(def die ({}) [] (callExtern exit 3))
-				(def main [] (die))
-			`,
-			output: `type :Str = {l, l, }
-type :GObject = {l, l, l, }
-data $_stdout = { l $stdout }
-function l $print(:Str %s) {
-@start
-	%str_data =l loadl %s
-	# 64-bit architecture only lul
-	%len_loc =l add %s, 8
-	%str_len =w loadw %len_loc
-	%stdout =l loadl $_stdout
-	%res =w call $fwrite(l %str_data, w 1, w %str_len, l %stdout)
-	ret 0
-}
-
-export function l $TestModule.die() {
-@start
-	%_tmp_1 =l call $exit (l 3)
-	ret %_tmp_1
-}
-export function w $main() {
-@start
-	%_tmp_2 =l call $TestModule.die ()
-	ret %_tmp_2
-}
-`,
-		},
-		{
-			desc: "module imports",
-			input: `
-				(import TestModule.AddOne)
-				(import TestModule)
-
-				(def inc [x] (AddOne.addOne x))
-
-				(def main []
-					(TestModule.hello))
-			`,
-			output: `type :Str = {l, l, }
-type :GObject = {l, l, l, }
-data $_stdout = { l $stdout }
-function l $print(:Str %s) {
-@start
-	%str_data =l loadl %s
-	# 64-bit architecture only lul
-	%len_loc =l add %s, 8
-	%str_len =w loadw %len_loc
-	%stdout =l loadl $_stdout
-	%res =w call $fwrite(l %str_data, w 1, w %str_len, l %stdout)
-	ret 0
-}
-
-export function l $TestModule.inc(l %x) {
-@start
-	%_tmp_1 =l call $AddOne.addOne (l %x)
-	ret %_tmp_1
-}
-export function w $main() {
-@start
-	%_tmp_2 =l call $TestModule.hello ()
-	ret %_tmp_2
-}
-`,
-		},
-	}
-	for _, tC := range testCases {
-		t.Run(tC.desc, func(t *testing.T) {
+		t.Run(name, func(t *testing.T) {
 			assert := assert.NewTestAsserts(t)
-			program, err := parse.ParseString(tC.input)
+			src, err := os.ReadFile("tests/" + entry.Name())
+			assert.Ok(err)
+
+			e, err := os.ReadFile("tests/" + name + ".qbe")
+			assert.Ok(err)
+			expect := string(e)
+
+			program, err := parse.ParseString(string(src))
 			assert.Ok(err)
 
 			mod := "MainModule"
@@ -219,12 +46,12 @@ export function w $main() {
 			Gen(&buf, mod, types[mod].TypeTree, program)
 
 			got := buf.String()
-			if got != tC.output {
-				t.Log("--- expected:")
-				t.Log(tC.output)
-				t.Log("--- got:")
-				t.Log(got)
-				t.Errorf("--- diff:\n%s", diffStr(t, tC.output, got))
+			if got != expect {
+				// t.Log("--- expected:")
+				// t.Log(expect)
+				// t.Log("--- got:")
+				// t.Log(got)
+				t.Errorf("--- diff:\n%s", diffStr(t, string(expect), got))
 			}
 		})
 	}
