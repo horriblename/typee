8543ebfcc3643ee55c6cf29240cdbbe483accb8b
Ching Pei Yang <badnam3o.0@gmail.com>
2025-02-24 12:12:02 +0100

    simplesub: add assertion for same uid different pointer

--- src/simplesub/typer.go
+++ src/simplesub/typer.go
@@ -1150,28 +1150,31 @@ func unify(lhs *Variable, rhs *Variable) error /*FIXME: idk what type*/ {
 	rep0 := lhs.Representative()
 	rep1 := rhs.Representative()
 
-	// FIXME: deep equality or pointer eq?
-	if rep0 != rep1 {
-		// NOTE: these occursCheck calls (and the following ones from addXBound) are pretty
-		// inefficient as they will incur repeated computation of type variables through getVars
-		if err := lhs.occursCheck(rep1.lowerBound, false); err != nil {
-			return err
-		}
-
-		if err := lhs.occursCheck(rep1.upperBound, true); err != nil {
-			return err
-		}
-
-		if err := rep1.newLowerBound(rep0.lowerBound); err != nil {
-			return err
-		}
-
-		if err := rep1.newUpperBound(rep0.upperBound); err != nil {
-			return err
-		}
-
-		rep0.representative = rep1
+	if rep0.Uid() == rep1.Uid() {
+		assert.Eq(rep0, rep1, "Same representative UID with different pointer value, uid:", rep0.Uid())
+		return nil
 	}
+
+	// NOTE: these occursCheck calls (and the following ones from addXBound) are pretty
+	// inefficient as they will incur repeated computation of type variables through getVars
+	if err := lhs.occursCheck(rep1.lowerBound, false); err != nil {
+		return err
+	}
+
+	if err := lhs.occursCheck(rep1.upperBound, true); err != nil {
+		return err
+	}
+
+	if err := rep1.newLowerBound(rep0.lowerBound); err != nil {
+		return err
+	}
+
+	if err := rep1.newUpperBound(rep0.upperBound); err != nil {
+		return err
+	}
+
+	rep0.representative = rep1
+
 	return nil
 }
 
