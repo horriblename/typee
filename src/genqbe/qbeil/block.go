a9c9bed9e49ad8625c0dd612dbcd9a75582e58bc
Ching Pei Yang <badnam3o.0@gmail.com>
2025-04-04 17:40:46 +0200

    qbeil: add jnz instruction

---
+++ src/genqbe/qbeil/block.go
@@ -0,0 +1,35 @@
+package qbeil
+
+import "fmt"
+
+// labels you can jump to @IDENT
+type Label struct {
+	Name string
+}
+
+func (self Label) IL() string {
+	return "@" + self.Name
+}
+
+func (b *Builder) TempLabel(namePrefix string) Label {
+	b.tempID++
+	if namePrefix == "" {
+		namePrefix = "_tmp_"
+	}
+	return Label{Name: fmt.Sprintf("%s%d", namePrefix, b.tempID)}
+}
+
+// jump if not zero
+func (b *Builder) Jnz(val Value, then Label, otherwise Label) {
+	b.indented([]byte("jnz "))
+	b.Buf.WriteString(val.IL())
+	b.Buf.WriteString(", ")
+	b.Buf.WriteString(then.IL())
+	b.Buf.WriteString(", ")
+	b.Buf.WriteString(otherwise.IL())
+}
+
+func (b *Builder) InsertLabel(l Label) {
+	b.Buf.Write([]byte(l.IL()))
+	b.Buf.WriteByte('\n')
+}
