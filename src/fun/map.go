227f11d3e0bfd7ed5fd50ba93df87af979a07864
Ching Pei Yang <badnam3o.0@gmail.com>
2025-11-30 00:24:29 +0100

    fun: add MapMapWithKeys

--- src/fun/map.go
+++ src/fun/map.go
@@ -7,3 +7,11 @@ func MapMap[K comparable, T, U any](m map[K]T, f func(T) U) map[K]U {
 	}
 	return out
 }
+
+func MapMapWithKeys[K comparable, T, U any](m map[K]T, f func(K, T) U) map[K]U {
+	out := map[K]U{}
+	for k, v := range m {
+		out[k] = f(k, v)
+	}
+	return out
+}
