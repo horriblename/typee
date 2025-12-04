6a34ea39a46fc9e5072bbbe1725a7de53664bc81
Ching Pei Yang <badnam3o.0@gmail.com>
2025-12-04 04:18:30 +0100

    qbeil: allow fmt in Comment

--- src/genqbe/qbeil/il.go
+++ src/genqbe/qbeil/il.go
@@ -145,8 +145,10 @@ func (b *Builder) Call(target *Var, typ ABIType, name Var, args []ABITypedValue)
 	}
 }
 
-func (b *Builder) Comment(s string) {
-	fmt.Fprintf(&b.Buf, "# %s\n", s)
+func (b *Builder) Comment(format string, args ...any) {
+	fmt.Fprint(&b.Buf, "# ")
+	fmt.Fprintf(&b.Buf, format, args...)
+	fmt.Fprintln(&b.Buf)
 }
 
 func (b *Builder) TempVar(global bool) Var {
