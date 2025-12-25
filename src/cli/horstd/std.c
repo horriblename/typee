8c8092e27c0a363de31c2fe9a46a3cc28643e3ad
Ching Pei Yang <badnam3o.0@gmail.com>
2025-12-25 18:03:23 +0100

    std: basic list impl

--- src/cli/horstd/std.c
+++ src/cli/horstd/std.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -20,3 +21,107 @@ char *strToCStr(Str s) {
 }
 
 void print(Str s) { fwrite(s.data, 1, s.size, stdout); }
+
+int32_t i64ToI32(int64_t x) { return x; }
+
+/*
+ * Lists
+ */
+
+// initial allocation cap if an allocation is required without a specified cap
+#define LIST_INITIAL_CAP 8
+#define LIST_GROWTH_RATE 2
+
+// PRINT_LIST(list, int, "%d");
+#define PRINT_LIST(l, Typ, fmt)                                                \
+  do {                                                                         \
+    for (int64_t i = 0; i < (l).size; i++) {                                   \
+      Typ _val;                                                                \
+      listGet(l, sizeof(Typ), i, &_val);                                       \
+      printf((fmt), _val);                                                     \
+    }                                                                          \
+    printf("\n");                                                              \
+  } while (0)
+
+// this goes on the heap
+typedef struct ListData {
+  int64_t ref_count;
+  int64_t size;
+  int64_t cap;
+  char data[];
+} ListData;
+
+// this goes on the stack
+typedef struct List {
+  int64_t size;
+  ListData *data;
+} List;
+
+List newList() {
+  List l = {.size = 0, .data = NULL};
+  return l;
+}
+
+List preallocateList(int64_t cap, int64_t item_size) {
+  // zero-out ListData.data?
+  ListData *data = calloc(sizeof(ListData) + cap * item_size, 1);
+  data->ref_count = 1;
+  data->size = 0;
+  data->cap = cap;
+  List l = {.size = 0, .data = data};
+  return l;
+}
+
+// returns bool actually
+static int64_t listDataHasSpace(List l) {
+  // a list has space for an append if:
+  // - the ListData.size < ListData.cap, and
+  // - the List.size == ListData.size (meaning no other slice appended to this)
+  //   (List.size < ListData.size is impossible right?)
+  return l.data && l.data->cap > l.data->size && l.size == l.data->size;
+}
+
+// I should figure out an "ABI" for passing generic values
+List listAppend(List l, int64_t item_size, void *thing) {
+  List newL;
+  if (listDataHasSpace(l)) {
+    l.data->size++;
+    memcpy(&l.data->data[l.size * item_size], thing, item_size);
+    newL.size = l.size + 1;
+    newL.data = l.data;
+    return newL;
+  }
+
+  if (!l.data) {
+    newL = preallocateList(LIST_INITIAL_CAP, item_size);
+    memcpy(&newL.data->data[l.size * item_size], thing, item_size);
+    newL.size++;
+    newL.data->size++;
+    return newL;
+  }
+
+  // someone else appended to the buffer, we copy it over but no need to
+  // grow buffer size
+  if (l.data->size < l.data->cap) {
+    newL = preallocateList(l.data->cap, item_size);
+  } else {
+    // buffer too small, grow it
+    newL = preallocateList(l.data->cap * LIST_GROWTH_RATE, item_size);
+  }
+
+  memcpy(newL.data->data, l.data->data, l.size * item_size);
+  newL.data->size = l.size + 1;
+  newL.size = l.size + 1;
+  memcpy(&newL.data->data[l.size * item_size], thing, item_size);
+  return newL;
+}
+
+void listGet(List l, int64_t item_size, int64_t i, void *result) {
+  assert(l.data);
+  assert(i < l.size && i < l.data->size);
+  memcpy(result, l.data->data + i * item_size, item_size);
+}
+
+int64_t len(List l) { return l.size; }
+
+void *emptyList() { return NULL; }
---
+++ src/cli/horstd/test.c
@@ -0,0 +1,113 @@
+#include "std.c"
+#include <stdio.h>
+
+#define TEST(msg, steps)                                                       \
+  do {                                                                         \
+    int _test_failed = 0;                                                      \
+    fprintf(stderr, "=============\nRunning test: %s\n", (msg));               \
+    steps;                                                                     \
+    if (_test_failed) {                                                        \
+      fprintf(stderr, "\x1b[31m FAIL \x1b[0m%s\n", (msg));                     \
+    } else {                                                                   \
+      fprintf(stderr, "\x1b[32m OK \x1b[0m\n");                                \
+    }                                                                          \
+  } while (0);
+
+#define CHECK(expr)                                                            \
+  if (!(expr)) {                                                               \
+    fprintf(stderr, "line %d: failed assertion: %s\n", __LINE__, #expr);       \
+    _test_failed = 1;                                                          \
+  }
+
+#define CHECK_EQ(lhs, rhs)                                                     \
+  if ((lhs) != (rhs)) {                                                        \
+    fprintf(stderr, "line %d: failed assertion: %s == %s\n", __LINE__, #lhs,   \
+            #rhs);                                                             \
+    fprintf(stderr, "  left:  %ld\n", (lhs));                                  \
+    fprintf(stderr, "  right: %ld\n", (rhs));                                  \
+    _test_failed = 1;                                                          \
+  }
+
+#define CHECK_NEQ(lhs, rhs)                                                    \
+  if ((lhs) == (rhs)) {                                                        \
+    fprintf(stderr, "line %d: failed assertion: %s != %s\n", __LINE__, #lhs,   \
+            #rhs);                                                             \
+    fprintf(stderr, "  left:  %ld\n", (lhs));                                  \
+    fprintf(stderr, "  right: %ld\n", (rhs));                                  \
+    _test_failed = 1;                                                          \
+  }
+
+int main() {
+  TEST("list append: empty buffer", {
+    List l = newList();
+    int item = 42;
+    CHECK_EQ(l.size, 0);
+    CHECK_EQ(l.data, NULL);
+
+    List l2 = listAppend(l, sizeof(item), &item);
+
+    CHECK_EQ(l2.size, 1);
+    CHECK(l2.data);
+    CHECK_EQ(l2.data->size, 1);
+    CHECK_EQ(l2.data->cap, LIST_INITIAL_CAP);
+    int get0;
+    listGet(l2, sizeof(int), 0, &get0);
+    CHECK_EQ(get0, item);
+  });
+
+  TEST("list append: append to already appended buffer", {
+    List l = newList();
+    int item0 = 1;
+    int item1 = 2;
+    int item2 = 3;
+    int result;
+
+    l = listAppend(l, sizeof(item0), &item0);
+    List l1 = listAppend(l, sizeof(item1), &item1);
+    List l2 = listAppend(l, sizeof(item2), &item2);
+
+    CHECK_EQ(l1.size, 2);
+    CHECK_EQ(l1.data->size, 2);
+    CHECK_EQ(l1.data->cap, LIST_INITIAL_CAP);
+
+    listGet(l1, sizeof(int), 0, &result);
+    CHECK_EQ(result, item0);
+
+    listGet(l1, sizeof(int), 1, &result);
+    CHECK_EQ(result, item1);
+
+    CHECK_NEQ(l1.data, l2.data);
+
+    CHECK_EQ(l2.size, 2);
+    CHECK_EQ(l2.data->size, 2);
+    CHECK_EQ(l2.data->cap, LIST_INITIAL_CAP);
+
+    listGet(l2, sizeof(int), 0, &result);
+    CHECK_EQ(result, item0);
+
+    listGet(l2, sizeof(int), 1, &result);
+    CHECK_EQ(result, item2);
+  });
+
+  TEST("list append: append to undersized buffer", {
+    int64_t initial_cap = 1;
+    List l = preallocateList(initial_cap, sizeof(int));
+    int item0 = 1;
+    int item1 = 2;
+
+    List l1 = listAppend(l, sizeof(item0), &item0);
+    List l2 = listAppend(l1, sizeof(item1), &item1);
+
+    CHECK_EQ(l2.size, 2);
+    CHECK_EQ(l2.data->size, 2);
+    CHECK_EQ(l2.data->cap, initial_cap * LIST_GROWTH_RATE);
+
+    int result0;
+    int result1;
+    listGet(l2, sizeof(int), 0, &result0);
+    listGet(l2, sizeof(int), 1, &result1);
+
+    CHECK_EQ(result0, item0);
+    CHECK_EQ(result1, item1);
+  })
+}
